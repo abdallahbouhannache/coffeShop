4a53eca06118bdfa6891a26266085636725850df
abdallahbouhannache <abdallah.bouhannache@gmail.com>
2020-03-21 01:42:31 +0100

    Form +Contact

--- src/App.js
+++ src/App.js
@@ -8,14 +8,70 @@ import imgsc from "./assets/dish.jpg";
 import altpic1 from "./assets/bg2.jpg";
 import altpic2 from "./assets/bg3.jpg";
 import altpic3 from "./assets/bg4.jpg";
+import Form from "./components/elements/Form";
+import Contact from "./components/elements/Contact";
 
 function App() {
+  const config = {
+    api: `${process.env.REACT_APP_API}`,
+    title: "Contact Me",
+    successMessage: "Thank you for contcting me.",
+    errorMessage: "Sorry we have some problems.",
+    fields: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      msg: ""
+    },
+    fieldsConfig: [
+      {
+        id: 1,
+        label: "First Name",
+        fieldName: "firstName",
+        type: "text",
+        placeholder: "Your First Name",
+        isRequired: true,
+        klassName: "first-name-field"
+      },
+      {
+        id: 2,
+        label: "Last Name",
+        fieldName: "lastName",
+        type: "text",
+        placeholder: "Your Last Name",
+        isRequired: true,
+        klassName: "last-name-field"
+      },
+      {
+        id: 3,
+        label: "Email",
+        fieldName: "email",
+        type: "email",
+        placeholder: " Your Email",
+        isRequired: true,
+        klassName: "email-field"
+      },
+      {
+        id: 4,
+        label: "Message",
+        fieldName: "msg",
+        type: "textarea",
+        placeholder: "Write something.....",
+        isRequired: true,
+        klassName: "message-field"
+      }
+    ]
+  };
   return (
     <div className="App">
       <Navbar />
       <SectionLayout imgsrc={[imgsc, altpic1, altpic2, altpic3]} />
 
-      <Carousel />
+      {/* <Carousel /> */}
+      <Contact />
+      <Form config={config} />
+      {/* <div className="test">
+      </div> */}
     </div>
   );
 }
---
+++ src/components/elements/Contact.js
@@ -0,0 +1,96 @@
+import React, { PureComponent } from "react";
+
+class Contact extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div class="ContactWrp">
+        <div class="half left contact-info">
+          <div class="small-title">Address</div>
+          <p>
+            345 Villeray street, Montreal, Québec H2R 1G8
+            <strong>514-439-9089</strong>
+          </p>
+          <div class="small-title">Business Hours</div>
+          <table class="business-hours" width="100%">
+            <tbody>
+              <tr>
+                <td class="business_hours_table_day">Monday</td>
+                <td
+                  class="business_hours_table_closed"
+                  colspan="2"
+                  align="center"
+                >
+                  Closed
+                </td>
+              </tr>
+
+              <tr>
+                <td class="business_hours_table_day">Tuesday</td>
+                <td class="business_hours_table_open">17:30</td>
+                <td class="business_hours_table_close">23:00</td>
+              </tr>
+
+              <tr>
+                <td class="business_hours_table_day">Wednesday</td>
+                <td class="business_hours_table_open">17:30</td>
+                <td class="business_hours_table_close">23:00</td>
+              </tr>
+
+              <tr>
+                <td class="business_hours_table_day">Thursday</td>
+                <td class="business_hours_table_open">17:30</td>
+                <td class="business_hours_table_close">23:00</td>
+              </tr>
+
+              <tr>
+                <td class="business_hours_table_day">Friday</td>
+                <td class="business_hours_table_open">17:30</td>
+                <td class="business_hours_table_close">23:00</td>
+              </tr>
+
+              <tr>
+                <td class="business_hours_table_day">Saturday</td>
+                <td class="business_hours_table_open">10:00</td>
+                <td class="business_hours_table_close">14:30 (brunch) </td>
+              </tr>
+
+              <tr>
+                <td class="business_hours_table_day"></td>
+                <td class="business_hours_table_open">17:00</td>
+                <td class="business_hours_table_close">23:00</td>
+              </tr>
+
+              <tr>
+                <td class="business_hours_table_day">Sunday</td>
+                <td class="business_hours_table_open">10:00</td>
+                <td class="business_hours_table_close">14:30 (brunch)</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div class="half right">
+          <h3 class="small-title">Directions</h3>
+          <h1 style={{ marginBottom: "20px" }}>
+            <img
+              src={require("../../assets/map.png")}
+              width="441"
+              height="300"
+              alt="Map"
+            />
+          </h1>
+          <h1>
+            Service de valet jeudi, vendredi et samedi soir offert par{" "}
+            <strong>Valet Québec</strong>.
+          </h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Contact;
